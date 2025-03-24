08bd005f75be875c375f7eeb6e7f99ec7dd4b1ad
Serban681 <46426268+Serban681@users.noreply.github.com>
2025-03-24 11:26:34 +0200

    Add files via upload

---
+++ USB_lab.cpp
@@ -0,0 +1,36 @@
+﻿#include <windows.h>
+#include <setupapi.h>
+#include <devguid.h>
+#include <regstr.h>
+#include <iostream>
+
+#pragma comment(lib, "setupapi.lib")
+
+void ListUSBDevices() {
+    HDEVINFO hDevInfo;
+    SP_DEVINFO_DATA DeviceInfoData;
+    DWORD i;
+
+    hDevInfo = SetupDiGetClassDevs(NULL, L"USB", 0, DIGCF_PRESENT | DIGCF_ALLCLASSES);
+    if (hDevInfo == INVALID_HANDLE_VALUE) {
+        std::cerr << "Failed to get device list.\n";
+        return;
+    }
+
+    DeviceInfoData.cbSize = sizeof(SP_DEVINFO_DATA);
+    for (i = 0; SetupDiEnumDeviceInfo(hDevInfo, i, &DeviceInfoData); i++) {
+        char deviceName[256];
+
+        if (SetupDiGetDeviceRegistryPropertyA(hDevInfo, &DeviceInfoData, SPDRP_FRIENDLYNAME,
+            NULL, (PBYTE)deviceName, sizeof(deviceName), NULL)) {
+            std::cout << "Device: " << deviceName << std::endl;
+        }
+    }
+
+    SetupDiDestroyDeviceInfoList(hDevInfo);
+}
+
+int main() {
+    ListUSBDevices();
+    return 0;
+}
